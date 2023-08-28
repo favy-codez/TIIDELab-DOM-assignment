ac4d4089237e1e3e2a599bde10f81ce77a9e991b
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-08-29 00:02:45 +0100

    added functionality

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+function setTheme(){
+    // getting the table
+    let attendanceTable = document.getElementById("attendance-table");
+    let selectedTheme = document.getElementById("themes");
+
+    if(selectedTheme.value === "blue"){
+        // setting the attribute
+        attendanceTable.setAttribute('class', 'blueTheme');
+    }else{
+        attendanceTable.setAttribute('class', 'redTheme');
+    }
+    console.log(selectedTheme.value);
+    
+}
