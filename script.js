6abc49a7e8d16af443dea96f3e7702641574e4bc
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-08-29 00:53:52 +0100

    added functionality

--- script.js
+++ script.js
@@ -18,7 +18,15 @@ function addRow(e){
     // override default behaviour
     e.preventDefault();
     // document.forms returns all the forms on the page,to differentiate we give the form a name="" or if you know their hierarchy use document.forms[0] to target the first form
-    let form =document.forms("attendanceForm");
-    // getting the table
-    let attendanceTable = document.getElementById("attendance-table");
+    let attendanceForm = document.forms["attendanceForm"];
+
+    // getting data using formData- it gives you everything filled in the form
+    let data = new FormData(attendanceForm);
+    console.log(data);
+
+    // getting the value of the input tags by ID
+    let firstName = document.getElementById("firstName").value;
+    console.log(firstName);
+    
+    
 }
